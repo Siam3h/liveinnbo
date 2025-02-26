e2c1d9297c104015b3e74a4d3e5c2cc39325495c
Siama <philosiama@gmail.com>
2025-02-27 02:00:19 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -111,11 +111,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database configuration (use one at a time)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL")
+        )
     }
-}
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
